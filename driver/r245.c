e08ccec244a54da96db49276b1a004d62036584e
bsv <bsv.serg@gmail.com>
2010-07-21 16:51:29 +0300

    add functions: R245_InitDev, R245_PacketForm, R245_UpdateCRC, R245_CRCCount,R245_PacketSend, R245_CorrectFA, R245_AuditEn, R245_GetVersion

---
+++ driver/r245.c
@@ -0,0 +1,242 @@
+#include "r245.h"
+
+const static unsigned short int crc_table[256] = {
+    0x0000, 0xc0c1, 0xc181, 0x0140, 0xc301, 0x03c0, 0x0280, 0xc241,
+    0xc601, 0x06c0, 0x0780, 0xc741, 0x0500, 0xc5c1, 0xc481, 0x0440,
+    0xcc01, 0x0cc0, 0x0d80, 0xcd41, 0x0f00, 0xcfc1, 0xce81, 0x0e40,
+    0x0a00, 0xcac1, 0xcb81, 0x0b40, 0xc901, 0x09c0, 0x0880, 0xc841,
+    0xd801, 0x18c0, 0x1980, 0xd941, 0x1b00, 0xdbc1, 0xda81, 0x1a40,
+    0x1e00, 0xdec1, 0xdf81, 0x1f40, 0xdd01, 0x1dc0, 0x1c80, 0xdc41,
+    0x1400, 0xd4c1, 0xd581, 0x1540, 0xd701, 0x17c0, 0x1680, 0xd641,
+    0xd201, 0x12c0, 0x1380, 0xd341, 0x1100, 0xd1c1, 0xd081, 0x1040,
+    0xf001, 0x30c0, 0x3180, 0xf141, 0x3300, 0xf3c1, 0xf281, 0x3240,
+    0x3600, 0xf6c1, 0xf781, 0x3740, 0xf501, 0x35c0, 0x3480, 0xf441,
+    0x3c00, 0xfcc1, 0xfd81, 0x3d40, 0xff01, 0x3fc0, 0x3e80, 0xfe41,
+    0xfa01, 0x3ac0, 0x3b80, 0xfb41, 0x3900, 0xf9c1, 0xf881, 0x3840,
+    0x2800, 0xe8c1, 0xe981, 0x2940, 0xeb01, 0x2bc0, 0x2a80, 0xea41,
+    0xee01, 0x2ec0, 0x2f80, 0xef41, 0x2d00, 0xedc1, 0xec81, 0x2c40,
+    0xe401, 0x24c0, 0x2580, 0xe541, 0x2700, 0xe7c1, 0xe681, 0x2640,
+    0x2200, 0xe2c1, 0xe381, 0x2340, 0xe101, 0x21c0, 0x2080, 0xe041,
+    0xa001, 0x60c0, 0x6180, 0xa141, 0x6300, 0xa3c1, 0xa281, 0x6240,
+    0x6600, 0xa6c1, 0xa781, 0x6740, 0xa501, 0x65c0, 0x6480, 0xa441,
+    0x6c00, 0xacc1, 0xad81, 0x6d40, 0xaf01, 0x6fc0, 0x6e80, 0xae41,
+    0xaa01, 0x6ac0, 0x6b80, 0xab41, 0x6900, 0xa9c1, 0xa881, 0x6840,
+    0x7800, 0xb8c1, 0xb981, 0x7940, 0xbb01, 0x7bc0, 0x7a80, 0xba41,
+    0xbe01, 0x7ec0, 0x7f80, 0xbf41, 0x7d00, 0xbdc1, 0xbc81, 0x7c40,
+    0xb401, 0x74c0, 0x7580, 0xb541, 0x7700, 0xb7c1, 0xb681, 0x7640,
+    0x7200, 0xb2c1, 0xb381, 0x7340, 0xb101, 0x71c0, 0x7080, 0xb041,
+    0x5000, 0x90c1, 0x9181, 0x5140, 0x9301, 0x53c0, 0x5280, 0x9241,
+    0x9601, 0x56c0, 0x5780, 0x9741, 0x5500, 0x95c1, 0x9481, 0x5440,
+    0x9c01, 0x5cc0, 0x5d80, 0x9d41, 0x5f00, 0x9fc1, 0x9e81, 0x5e40,
+    0x5a00, 0x9ac1, 0x9b81, 0x5b40, 0x9901, 0x59c0, 0x5880, 0x9841,
+    0x8801, 0x48c0, 0x4980, 0x8941, 0x4b00, 0x8bc1, 0x8a81, 0x4a40,
+    0x4e00, 0x8ec1, 0x8f81, 0x4f40, 0x8d01, 0x4dc0, 0x4c80, 0x8c41,
+    0x4400, 0x84c1, 0x8581, 0x4540, 0x8701, 0x47c0, 0x4680, 0x8641,
+    0x8201, 0x42c0, 0x4380, 0x8341, 0x4100, 0x81c1, 0x8081, 0x4040
+};
+
+static unsigned char packet_ctr = 0; // packet counter
+static unsigned char rx_buffer[BUFFER_LEN];
+static unsigned char tx_buffer[BUFFER_LEN]; // Contains data to write to device
+
+unsigned long R245_InitDev(short int dev_number, void **ft_handle)
+{
+    unsigned long ft_status;
+    
+    if (ft_status = FT_Open(0, ft_handle) != FT_OK) {
+        return ft_status;
+    }
+    
+    ft_status = FT_SetBaudRate(*ft_handle, 9600);
+    if (ft_status != FT_OK) {
+        return ft_status;
+    }
+
+    ft_status = FT_SetDataCharacteristics(ft_handle, FT_BITS_8, FT_STOP_BITS_1,
+    FT_PARITY_NONE);
+    if (ft_status != FT_OK) {
+        return ft_status;
+    }
+
+    return R245_OK;
+}
+
+void R245_UpdateCRC(short int *crc, char byte)
+{
+    *crc = (short int) (((*crc >> 8) & 0xff) ^ crc_table[(*crc ^ byte) & 0xff]);
+}
+
+short int R245_PacketForm(unsigned char dev_addr, unsigned short int cmd,
+    unsigned char * data, unsigned char data_len, unsigned char *packet_out)
+{
+    
+    unsigned char packet[MAX_PACKET_LEN];
+    unsigned char packet_len = 0;
+    unsigned short int crc = 0;
+    int i = 0;
+
+    packet[0] = 0xFB; //start
+    packet[1] = packet_ctr; // number of packet
+    packet[2] = dev_addr; // address device
+    packet[3] = 0x00;
+    packet[4] = cmd;
+    packet[5] = data_len; // len
+
+    for(packet_len = PACKET_HEAD_LEN; packet_len < data_len+PACKET_HEAD_LEN; packet_len++)
+    {
+        packet[packet_len] = data[packet_len - PACKET_HEAD_LEN];
+    }
+
+    crc = R245_CRCCount(INITIAL_CRC, &packet[1], packet_len-1);// crc without start bit (i-1)
+
+    packet[packet_len++] = crc;
+    packet[packet_len++] = crc >> 8;
+    packet[packet_len++] = 0xFD; // stop bit
+    
+    if(R245_CorrectFA(packet, packet_len, packet_out) == R245_OK)
+    {  
+        return R245_OK;
+    } else {
+        return R245_ERROR;
+    }
+
+}
+
+short int R245_CorrectFA(unsigned char * packet, unsigned char packet_len,
+        unsigned char *packet_out)
+{
+    unsigned char i;
+    unsigned short int j = 0;
+
+    for(i = 0; i < packet_len; i++)
+    {
+        if((packet[i] >= 0xFA) && (i != 0) && (i != packet_len-1))
+        {
+            packet_out[j++] = 0xFA;
+            packet_out[j++] = packet[i] - 0xF0;
+        }
+        else
+        {
+            packet_out[j++] = packet[i];
+        }
+        if(j >= BUFFER_LEN)
+            return R245_ERROR;
+    }
+
+    return R245_OK;
+}
+
+short int R245_CRCCount(short int init_crc, char * data, int len)
+{
+    short int crc;
+    char * u8_data;
+
+    u8_data = data;
+    crc    = init_crc;
+    
+    while (len--) {
+        R245_UpdateCRC(&crc, *u8_data++);
+    }
+    return crc;
+}
+
+short int R245_PacketSend(void * ft_handle, unsigned char dev_addr, unsigned short int cmd,
+    unsigned char * data, unsigned char data_len)
+{
+    unsigned long ft_status, bytes_written, bytes_received, rx_bytes, tx_bytes;
+    short int tx_packet_len = data_len + PACKET_HEAD_LEN + PACKET_END_LEN;
+    short int rx_packet_len = 0;
+    void * h_event;
+    unsigned long event_mask, event_dword;
+
+    R245_PacketForm(dev_addr, cmd, data, data_len, tx_buffer);
+
+    ft_status = FT_Write(ft_handle, tx_buffer, tx_packet_len, &bytes_written);
+    if (ft_status == FT_OK) {
+        printf("bytes = %d\n", bytes_written);
+    }
+    else {
+        printf("Error: write");
+        
+        return R245_ERROR;
+    }
+
+    h_event = CreateEvent(
+          NULL,
+          0, // auto-reset event
+          0, // non-signalled state
+          ""
+    );
+    
+    event_mask = FT_EVENT_RXCHAR;
+    ft_status = FT_SetEventNotification(ft_handle, event_mask, h_event);
+
+    rx_buffer[N_DATA_LEN] = 0;
+    
+    while(rx_packet_len <= N_DATA_LEN+1 || rx_packet_len !=
+                        rx_buffer[N_DATA_LEN]+PACKET_HEAD_LEN + PACKET_END_LEN)
+    {
+        if(WaitForSingleObject(h_event, 0xFF/*INFINITE*/))
+        {
+            printf("Error: wait recieve data\n");
+            return R245_ERROR;
+        }
+
+        FT_GetStatus(ft_handle, &rx_bytes,&tx_bytes,&event_dword);
+        if (rx_bytes > 0)
+        {
+
+            printf("READ bytes %d\n", rx_bytes);
+
+            ft_status = FT_Read(ft_handle, &rx_buffer[rx_packet_len], 
+            rx_bytes, &bytes_received);
+
+            rx_packet_len += bytes_received;
+
+            if(ft_status != FT_OK)
+                break;
+        }
+    }
+
+    if (ft_status == FT_OK && rx_buffer[N_CMD_RES] == R245_RES_OK)
+    {
+       packet_ctr++;
+       printf("Send ok\n");
+       return R245_OK;
+    }
+    else
+    {
+        printf("Error: read");
+        return R245_ERROR;
+    }
+
+    return R245_OK;
+}
+
+short int R245_AuditEn(void * ft_handle, unsigned char dev_addr,
+        unsigned char enable)
+{
+    return R245_PacketSend(ft_handle, dev_addr, AUDIT_EN, &enable, 1);
+}
+
+short int R245_GetVersion(void * ft_handle, unsigned char dev_addr,
+        unsigned char *version)
+{
+    void *data;
+    short int i = 0;
+    unsigned long ft_status;
+
+    ft_status = R245_PacketSend(ft_handle, dev_addr, GET_VERSION, data, 0);
+    if(ft_status == FT_OK)
+    {
+        // копирование можно заменить стандартной функцией библиотеки
+        for(i = 0; i < rx_buffer[N_DATA_LEN]; i++)
+        {
+            version[i] = rx_buffer[i + PACKET_HEAD_LEN];
+        }
+        version[i] = '\0';
+    }
+
+    return ft_status;
+}
+
--- driver/r245.h
+++ driver/r245.h
@@ -1,31 +1,77 @@
-#define GET_VERSION 	0x28
-#define SET_TIME		0xC9
-#define SET_DATE		0xCA
-#define GET_TIME		0xCB
-#define GET_DATE		0xCC
-#define SET_ADRESS		0x1E
-#define GET_SENSORS		0xB6
+#ifndef _R245_H
+#define	_R245_H
+
+#include "ftd2xx.h"
+
+// CMD CODE=====================================================================
+#define GET_VERSION     0x28
+#define SET_TIME        0xC9
+#define SET_DATE        0xCA
+#define GET_TIME        0xCB
+#define GET_DATE        0xCC
+#define SET_ADRESS      0x1E
+#define GET_SENSORS     0xB6
 #define GET_NUM_TAGS_1	0x50
 #define GET_FIRST_TAG_1	0x51
 #define GET_NEXT_TAG_1	0x52
 #define GET_NUM_TAGS_2	0x53
 #define GET_FIRST_TAG_2	0x54
 #define GET_NEXT_TAG_2	0x55
-#define SET_DAMP_1		0x0C
-#define SET_DAMP_2		0x0D
-#define GET_DAMP_1		0x12
-#define GET_DAMP_2		0x13
-#define SET_TIME_1		0x19
-#define SET_TIME_2		0x1A
-#define GET_TIME_1		0x1B
-#define GET_TIME_2		0x1C
-#define SET_CHAN_1		0xCD
-#define SET_CHAN_2		0xCE
-#define AUDIT_EN		0xB5
-#define CLEAR_TRANS		0x78
-#define GET_NUM_TRANS	0x79
-#define GET_TRANS		0x7A
+#define SET_DAMP_1      0x0C
+#define SET_DAMP_2      0x0D
+#define GET_DAMP_1      0x12
+#define GET_DAMP_2      0x13
+#define SET_TIME_1      0x19
+#define SET_TIME_2      0x1A
+#define GET_TIME_1      0x1B
+#define GET_TIME_2      0x1C
+#define SET_CHAN_1      0xCD
+#define SET_CHAN_2      0xCE
+#define AUDIT_EN        0xB5
+#define CLEAR_TRANS     0x78
+#define GET_NUM_TRANS   0x79
+#define GET_TRANS       0x7A
+#define INITIAL_CRC     0x6363
+//==============================================================================
 
-void PacketForm(unsigned short int cmd, unsigned short int datalen, unsigned short int data, void * packet);
-void CRCCount(int init_crc, void * data, int len);
-void PacketSend(void * data, unsigned short int adress);
+#define MAX_PACKET_LEN          256
+#define BUFFER_LEN              256
+#define PACKET_HEAD_LEN         6
+#define PACKET_END_LEN          3
+#define R245_OK                 0
+#define R245_ERROR              1
+
+//Error code====================================================================
+#define R245_RES_OK             0x00  //Операция выполнена успешно
+#define R245_ERR_WRONG_PARAM    0x20  //Неправильное число параметров
+#define R245_ERR_INVALID_DATA   0x21  //Недопустимое значение данных
+#define R245_ERR_NO_TAG         0x80  //Нет тага в поле считывателя
+#define R245_ERR_UNKNOWN        0x200 //Неизвестная ошибка
+#define R245_ERR_NO_IMLEMENT    0x201 //Команда не поддерживается
+//==============================================================================
+
+#define N_START_BIT         0
+#define N_PACKET_NUMBER     1
+#define N_DEV_ADDR          2
+#define N_PROTOCOL_OPTION   3
+#define N_CMD_RES           4
+#define N_DATA_LEN          5
+#define N_CRC               6
+#define N_END_BIT           7
+
+
+unsigned long R245_InitDev(short int dev_number, void ** ft_handle);
+short int R245_PacketForm(unsigned char dev_addr, unsigned short int cmd,
+    unsigned char * data, unsigned char data_len, unsigned char * packet_out);
+void R245_UpdateCRC(short int *crc, char byte);
+short int R245_CRCCount(short int init_crc, char * data, int len);
+short int R245_PacketSend(void * ft_handle, unsigned char dev_addr,
+        unsigned short int cmd, unsigned char * data, unsigned char data_len);
+short int R245_CorrectFA(unsigned char * packet, unsigned char packet_len,
+        unsigned char *packet_out);
+short int R245_AuditEn(void * ft_handle, unsigned char dev_addr, unsigned char enable);
+short int R245_GetVersion(void * ft_handle, unsigned char dev_addr,
+        unsigned char *version);
+
+#endif	/* _R245_H */
+
